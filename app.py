cc152b0bd84ae4dbb0dc545e238aa8a433ee4a65
Marcelo Serpa <jdev.marcelo@gmail.com>
2023-03-11 08:26:41 -0300

    force error (#12)

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World Marcelo Serpa" 
+    return "Lab: Marcelo Serpa"
+
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 
 if __name__ == '__main__':
     app.run()
